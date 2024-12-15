0badac76b6bc96f9e8e0b3f074127717b3b6d601
Timothy <tlzw2003@gmail.com>
2024-12-16 03:39:35 +0800

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -39,6 +39,8 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
+            <p>Hi, I juse made some changes!</p>
+
           </Link>
         );
       })}
